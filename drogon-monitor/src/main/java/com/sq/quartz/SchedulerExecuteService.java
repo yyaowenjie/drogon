50d8e3879e66c1d0a6943fce699dc7b5fde9503f
yaowenjie@sinieco.com <yaowenjie@sinieco.com>
2016-07-15 13:23:05 +0800

    drogon

---
+++ drogon-monitor/src/main/java/com/sq/quartz/SchedulerExecuteService.java
@@ -0,0 +1,21 @@
+package com.sq.quartz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 定时任务执行业务类.
+ * User: yaowenjie
+ * Date: 2016/6/15
+ * Time: 09:26
+ */
+@Service
+public class SchedulerExecuteService {
+
+    private static final Logger log = LoggerFactory.getLogger(SchedulerExecuteService.class);
+
+    public void checkAllProjectPoint(){
+        log.error("test!!!!");
+    }
+}
